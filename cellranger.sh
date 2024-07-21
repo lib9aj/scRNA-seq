3c754db7259dd68943c2d223f704c24083ec1a9e
lib9aj <151455422+lib9aj@users.noreply.github.com>
2024-07-20 23:22:41 -0400

    Delete cellranger.sh

--- cellranger.sh
+++
@@ -1,3 +0,0 @@
-# bsub -L /bin/bash -W 40:00 -n 4 -R "span[ptile=4]" -M 128000 -e %J.err -o %J.out ./cellranger_v5.sh
-module load cellranger/5.0.1
-cellranger count --id=CRR794362 --transcriptome=/database/cellranger/mm10-v2 --fastqs=./fastqs
