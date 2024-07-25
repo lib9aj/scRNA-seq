1e76cdf7c7e55d83b5e4fc5b3cd8703d05acf762
lib9aj <151455422+lib9aj@users.noreply.github.com>
2024-07-24 22:10:46 -0400

    Update pbmc.R

--- pbmc/pbmc.R
+++ pbmc/pbmc.R
@@ -1,3 +1,7 @@
+
+
+### Setup the Seurat Object
+
 library(dplyr)
 library(Seurat)
 library(patchwork)
@@ -20,10 +24,10 @@ print(sparse.size)
 dense.size / sparse.size
 
 
+### Standard pre-processing workflow
 
 # The [[ operator can add columns to object metadata. This is a great place to stash QC stats
 pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
-
 
 # Show QC metrics for the first 5 cells
 head(pbmc@meta.data, 5)
@@ -39,16 +43,19 @@ plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
 
-
 pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
 
-## pbmc <- NormalizeData(pbmc)
+
+### Normalizing the data
+
+# this step is equal to pbmc <- NormalizeData(pbmc)
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 pbmc[["RNA"]]$data
 
 
 
+### Identification of highly variable features (feature selection)
 pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
 
 # Identify the 10 most highly variable genes
@@ -56,6 +63,77 @@ top10 <- head(VariableFeatures(pbmc), 10)
 
 # plot variable features with and without labels
 plot1 <- VariableFeaturePlot(pbmc)
-plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE, xnudge = 0, ynudge = 0)
 plot1 + plot2
- 
+
+
+### Scaling the data
+
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+
+pbmc[["RNA"]]$scale.data
+
+
+### Perform linear dimensional reduction
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+
+# Examine and visualize PCA results a few different ways
+#1
+print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
+#2
+VizDimLoadings(pbmc, dims = 1:2, reduction = "pca")
+#3
+DimPlot(pbmc, reduction = "pca") + NoLegend()
+#4
+DimHeatmap(pbmc, dims = 1, cells = 500, balanced = TRUE)
+DimHeatmap(pbmc, dims = 1:15, cells = 500, balanced = TRUE)
+
+
+###Determine the ‘dimensionality’ of the dataset
+
+
+ElbowPlot(pbmc)
+
+# another way--- Perform JackStraw analysis to assess the significance of principal components
+pbmc <- JackStraw(pbmc, num.replicate = 100)
+pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
+JackStrawPlot(pbmc, dims = 1:20)
+
+###Cluster the cells
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+
+# Look at cluster IDs of the first 5 cells
+head(Idents(pbmc), 5) 
+
+
+###Run non-linear dimensional reduction (UMAP/tSNE)
+
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+
+# note that you can set `label = TRUE` or use the LabelClusters function to help label
+# individual clusters
+DimPlot(pbmc, reduction = "umap")
+#You can save the object at this point so that it can easily be loaded back in without having to rerun the computationally intensive steps performed above, or easily shared with collaborators.
+ls()
+rm(PBMC_data,P,p2,pbmc_small)
+ls()
+saveRDS(pbmc, file = "/Users/lib9aj/pbmc_tutorial.rds")
+
+
+###Finding differentially expressed features (cluster biomarkers)
+# find all markers of cluster 2
+cluster2.markers <- FindMarkers(pbmc, ident.1 = 2)
+head(cluster2.markers, n = 5)
+
+# find all markers distinguishing cluster 5 from clusters 0 and 3
+cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3))
+head(cluster5.markers, n = 5)
+
+# find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE)
+pbmc.markers %>%
+  group_by(cluster) %>%
+  dplyr::filter(avg_log2FC > 1)
