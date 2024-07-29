3e96d575db0b09f36f2230bd868334b747d4fdfb
lib9aj <151455422+lib9aj@users.noreply.github.com>
2024-07-28 20:47:40 -0400

    Update pbmc.R

--- pbmc/pbmc.R
+++ pbmc/pbmc.R
@@ -18,7 +18,6 @@
 library(dplyr)
 library(Seurat)
 library(patchwork)
-
 # Load the PBMC dataset
 pbmc.data <- Read10X ("/Users/lib9aj/git/scRNA-seq/pbmc/filtered_gene_bc_matrices/hg19")
 # Initialize the Seurat object with the raw (non-normalized data).
@@ -49,7 +48,6 @@ VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3
 
 # FeatureScatter is typically used to visualize feature-feature relationships, but can be used
 # for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
-
 plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
 plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
 plot1 + plot2
@@ -58,12 +56,10 @@ pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent
 
 
 ###(3) Normalizing the data
-
 # this step is equal to pbmc <- NormalizeData(pbmc)
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 pbmc[["RNA"]]$data
-
 
 
 ###(4) Identification of highly variable features (feature selection)
@@ -127,7 +123,6 @@ head(Idents(pbmc), 5)
 
 
 ###(9) Run non-linear dimensional reduction (UMAP/tSNE)
-
 pbmc <- RunUMAP(pbmc, dims = 1:10)
 
 # note that you can set `label = TRUE` or use the LabelClusters function to help label
@@ -166,8 +161,7 @@ VlnPlot(pbmc, features = c("MS4A1", "CD79A"))
 VlnPlot(pbmc, features = c("NKG7", "PF4"), slot = "counts", log = TRUE)
 
 
-FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP",
-                               "CD8A"))
+FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP", "CD8A"))
 #DoHeatmap() generates an expression heatmap for given cells and features. In this case, we are plotting the top 20 markers (or all markers if less than 20) for each cluster.
 pbmc.markers %>%
   group_by(cluster) %>%
